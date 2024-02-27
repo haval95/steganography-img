6c1e23afad5c068c565907deed861adad2602cf4
haval95 <havalrezan22@gmail.com>
2024-02-27 19:09:02 +0100

    method to convert binary to text

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 def text_to_binary(text):
     binary_code = "".join(format(ord(char), "08b") for char in text)
     return binary_code
+
+
+def binary_to_text(biarny):
+    return "".join(chr(int(biarny[i : i + 8], 2)) for i in range(0, len(biarny), 8))
