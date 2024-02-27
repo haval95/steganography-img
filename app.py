8c46b1e5b41b96766c16966bf2d4d3b3357e4aa7
haval95 <havalrezan22@gmail.com>
2024-02-27 19:09:55 +0100

    a method to check if the given message can fit the image

--- app.py
+++ app.py
@@ -5,3 +5,10 @@ def text_to_binary(text):
 
 def binary_to_text(biarny):
     return "".join(chr(int(biarny[i : i + 8], 2)) for i in range(0, len(biarny), 8))
+
+
+# Check if the message can be embedded in the image
+def check_fitness(message, pixels):
+    message_length = len(message)
+    if message_length > pixels:
+        raise ValueError("Message is too large to be embedded in the image.")
